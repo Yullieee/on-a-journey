00e2804e9e0e136c5ff760340ab5715443699ad3
yulle <maudedamour@gmail.com>
2023-09-29 17:00:37 +0200

    brain hurts :(

---
+++ codewars/day_two/prime_factor.js
@@ -0,0 +1,43 @@
+function isPrime(num) {                                         //yay we can use the function we previously made for this kata!! c:
+
+    if (num % 2 === 0 && num !== 2 ) { return false }
+    if (num <= 1) { return false }
+    if (num === 2) { return true }
+
+    let limit = Math.sqrt(num)+1;
+
+    for (let k = 3 ; k <= limit ; k+=2) {
+        if (num % k === 0) { return false }
+    }
+    return true;
+}
+
+function primeFactors(n) {
+
+    if (isPrime(n)) { return "(" + n + ")"; }                   //we check if the given number is already a prime and return in the requested format
+
+    let result = "";
+
+    for (let i = 2; i <= n/2; i++) {                            //we go through every number ranged from 1 to the half of the given number
+        if (isPrime(i) && n % i === 0) { //                     //we check if i is a prime and if it is we check if n can be divided by it
+            let count = 0;
+            while (n % i === 0) {                               //we initialize a while so that as long as i divided n we keep going and count each time it does
+                count += 1;
+                n /= i;
+            }
+            if (count > 1) {
+                result = result + "(" + i + "**" + count + ")"; //count is actually the power of i
+            } else { 
+                result = result + "(" + i + ")";            
+            }
+        }
+    }
+    return result;
+}
+
+
+const n = 544;
+console.log(primeFactors(n));
+
+
+
