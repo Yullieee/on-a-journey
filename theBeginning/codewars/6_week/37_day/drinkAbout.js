4ad9083fac20cc0f4b5b06a805d00de956ac365d
yulle <maudedamour@gmail.com>
2023-11-03 23:00:59 +0100

    idk

---
+++ theBeginning/codewars/6_week/37_day/drinkAbout.js
@@ -0,0 +1,16 @@
+/*
+
+Make a function that receive age, and return what they drink.
+
+Rules:
+
+Children under 14 old.
+Teens under 18 old.
+Young under 21 old.
+Adults have 21 or more.
+
+*/
+
+function peopleWithAgeDrink(old) {
+  return (old < 14 ? "drink toddy" : (old < 18 ? "drink coke" : (old < 21 ? "drink beer" : "drink whisky")));
+};
